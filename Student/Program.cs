0a8400d568dcb21351d5d9c107b8790ae358cb9c
Tatia Chichinadze <echichinadze@credo.ge>
2024-10-29 22:23:10 +0400

    Add project files.

---
+++ Student/Program.cs
@@ -0,0 +1,13 @@
+﻿
+string[] students = new string[3];
+
+for (int i = 0; i < students.Length; i++)
+{
+    Console.WriteLine($"Enter students name {i + 1};");
+    students[i] = Console.ReadLine();
+}
+Console.WriteLine("List of students");
+for (int i = 0; i < students.Length; i++)
+{
+    Console.WriteLine($"{i + 1} {students[i]}");
+}
